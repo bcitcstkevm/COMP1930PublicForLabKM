3524efdcbb19abfdbab581ed5edda67d6c96c6b2
Kevin Mark <kmark15@my.bcit.ca>
2018-12-05 16:08:40 -0800

    test

---
+++ init_firebase.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB2O8pk2xx8HkCoFLwL5v2Ln_b4Q63OW8k",
+    authDomain: "website-b059f.firebaseapp.com",
+    databaseURL: "https://website-b059f.firebaseio.com",
+    projectId: "website-b059f",
+    storageBucket: "website-b059f.appspot.com",
+    messagingSenderId: "1079994940223"
+};
+firebase.initializeApp(config);
+// database variable.
+var database = firebase.database();
---
+++ ques.js
@@ -0,0 +1,31 @@
+let results = document.getElementsByTagName('input');
+let results_checked = {};
+
+function update_database() {
+    for (let i = 0; i < results.length; i++) {
+        if (results[i].checked) {
+            results_checked[results[i].name] = (results[i].value)
+        }
+    }
+
+    let user_id = window.localStorage.getItem('person')
+
+    var promise = firebase.database().ref().child('users/' + user_id).set({
+        rating: get_rating()
+    });
+
+    promise.then(() => {
+        window.location = 'main.html'
+    })
+}
+
+function get_rating() {
+    let total = 0;
+    Object.keys(results_checked).forEach(function (key) {
+
+        total += parseInt(results_checked[key])
+        // console.log(key, results_checked[key]);
+    });
+
+    return total;
+}
